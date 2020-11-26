a3fe7a12bbb18f24110f14d5dd8778db2c588f2b
Daisy <1210194312@qq.com>
2020-11-26 17:03:05 +0800

    闭包{closureParameter->statement}学习

---
+++ src/test/Groovy/com/github/ClosureStudy.groovy
@@ -0,0 +1,36 @@
+package com.github
+
+import org.junit.Test
+
+class ClosureStudy {
+    def firstClosure = {println("Hello World")} //把闭包赋值给变量，闭包使用{}，{closureParameter->statement}, closure指闭包接受的参数，多个参数用逗号隔开，statement指闭包中的代码
+    def secondClosure = {a, b -> a + b}
+
+    @Test
+    void testClosure() {
+        firstClosure()  //运行闭包
+        println secondClosure(1, 2)
+    }
+
+    def myFunction(name,closure) {
+        closure(name)
+    }
+
+    @Test
+    void testMyFunction() {
+        myFunction("Dave",{name->println("my name is ${name}")})
+        myFunction("Rave",{it->println("my name is ${it}")})
+        myFunction("Lave",{println("my name is ${it}")})
+    }
+
+    void function(closure) {
+        def a = "hello"
+        closure(a)
+    }
+
+    @Test
+    void testFunction() {
+        function({it->println(it)})
+        function{it->println(it)}
+    }
+}
