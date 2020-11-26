be22b84f608a1fd5ccc00c8b8fa669135588ee2a
Daisy <1210194312@qq.com>
2020-11-26 17:31:39 +0800

    学习了find（）和findall（）

--- src/test/Groovy/com/github/ClosureStudy.groovy
+++ src/test/Groovy/com/github/ClosureStudy.groovy
@@ -35,4 +35,30 @@ class ClosureStudy {
         function({it->println(it)})
         function{it->println(it)}
     }
+
+    @Test
+    void testEach() {
+        def firstList = ["zhangsan", 1, 2, 4, "lisi"]
+        firstList.each {println it}
+
+        //求和
+        def secondeList = [1, 3, 5, 7, 8, 10]
+        def a = 0
+        secondeList.each {a = a + it}
+        println(a)
+
+        //map对象的each方法
+        def myMap = ["name":"tom","age":28]
+        myMap.each {key,value->println(key + "---" + value)}
+    }
+
+    @Test
+    void testFind() {
+        def firstList = [1, 3, 5, 8, 9]
+        def result = firstList.find({it->it > 5})
+        println(result)
+
+        result = firstList.findAll({it -> it > 5})
+        println result
+    }
 }
